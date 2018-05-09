a6322ea24dbced971839af9f302d8ad0af2de69e
BitrustProject <38917775+BitrustProject@users.noreply.github.com>
2018-05-09 20:33:40 +0800

    add token test file

---
+++ ttest.sol
@@ -0,0 +1,41 @@
+import "./token.sol";
+
+contract ttest
+{
+    event LogUint(uint value);
+    BiTrustToken token;
+    function () payable public
+    {
+        
+    }
+    function ttest() public
+    {
+        token = new BiTrustToken(1000000, "DDD", "DD");
+    }
+    function testBuy()  public payable
+    {
+        uint tamount = token.buy.value(1 ether)();
+        assert(tamount == 100 ether);
+    }
+    
+    function testSell() public 
+    {
+        uint oldb = address(this).balance;
+        uint oldtb = token.balanceOf(this);
+        token.sell(1 ether);
+        uint newb =  address(this).balance;
+        uint newtb = token.balanceOf(this);
+        assert((oldtb - newtb) == 1 ether);
+        assert((newb - oldb) == 0.01 ether);
+    }
+    function testMint() public
+    {
+        uint oldb = token.balanceOf(this);
+        token.mintToken(this, 100 ether);
+        uint newb = token.balanceOf(this);
+        
+        emit LogUint(oldb);
+        emit LogUint(newb);
+        assert((newb - oldb) == 100 ether);
+    }
+}
